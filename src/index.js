5c98559f830213e8a564b769ca5a270c9d139e68
Ivan Amoshyi <amoshyiivan@gmail.com>
2025-07-29 16:21:31 +0300

    Released language selection

--- src/index.js
+++ src/index.js
@@ -1,14 +1,81 @@
 export default {
 	async fetch(request, env) {
 		console.log("Worker received request with method:", request.method);
-
 		try {
 			const update = await request.json();
 			const message = update?.message?.text;
-			const chatId = update?.message?.chat?.id;
+			const chatId =
+				update?.message?.chat?.id ?? update?.callback_query?.message?.chat?.id;
+
+
+			const callbackQuery = update.callback_query;
+			const data = callbackQuery?.data;
+			const callbackId = callbackQuery?.id;
+
+			const target_lang = await env.LANG_STORAGE.get(chatId.toString()) || "UK";
+
+			if (callbackId) {
+				if (!chatId) {
+					return new Response("No chatId for callback", { status: 400 });
+				}
+				await env.LANG_STORAGE.put(chatId.toString(), data);
+
+				await fetch(
+					`https://api.telegram.org/bot${env.BOT_TOKEN}/answerCallbackQuery`,
+					{
+						method: "POST",
+						headers: { "Content-Type": "application/json" },
+						body: JSON.stringify({
+							callback_query_id: callbackId,
+							text: `Selected language: ${data}`,
+							show_alert: false,
+						}),
+					}
+				);
+
+				return new Response("OK");
+			}
+
 
 			if (!message || !chatId) {
 				return new Response("No message or chatId", { status: 400 });
+			}
+
+			// Selecting translated language
+			if (message === "/language") {
+				const languagelist = await fetch(
+					"https://api-free.deepl.com/v2/languages",
+					{
+						headers: {
+							Authorization: `DeepL-Auth-Key ${env.DEEPL_API_KEY}`,
+						},
+					}
+				).then((res) => res.json());
+
+
+				await fetch(
+					`https://api.telegram.org/bot${env.BOT_TOKEN}/sendMessage`,
+					{
+						method: "POST",
+						headers: { "Content-Type": "application/json" },
+						body: JSON.stringify({
+							chat_id: chatId,
+							text: "List of supported languages:",
+							reply_markup: {
+								inline_keyboard: languagelist
+									.filter(item => item.language !== "RU")
+									.map((item) => [
+									{
+										text: item.name,
+										callback_data: item.language,
+									},
+								]),
+							},
+						}),
+					}
+				);
+
+				return new Response("OK");
 			}
 
 			const translated = await fetch(
@@ -21,7 +88,7 @@ export default {
 					},
 					body: new URLSearchParams({
 						text: message,
-						target_lang: "UK",
+						target_lang,
 					}),
 				}
 			).then((res) => res.json());
