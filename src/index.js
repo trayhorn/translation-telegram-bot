b454d188cafcbb4e1e8c83d5f54b1da944257247
Ivan Amoshyi <amoshyiivan@gmail.com>
2025-08-26 14:45:25 +0300

    Released option to save the word to the dictionary

--- src/index.js
+++ src/index.js
@@ -7,10 +7,11 @@ export default {
 			const chatId =
 				update?.message?.chat?.id ?? update?.callback_query?.message?.chat?.id;
 
-
 			const callbackQuery = update.callback_query;
-			const data = callbackQuery?.data;
 			const callbackId = callbackQuery?.id;
+			const callbackData = callbackQuery?.data
+				? JSON.parse(callbackQuery.data)
+				: null;
 
 			const target_lang = await env.LANG_STORAGE.get(chatId.toString()) || "UK";
 
@@ -18,24 +19,57 @@ export default {
 				if (!chatId) {
 					return new Response("No chatId for callback", { status: 400 });
 				}
-				await env.LANG_STORAGE.put(chatId.toString(), data);
+				if (callbackData.action === "set_language") {
+					await env.LANG_STORAGE.put(chatId.toString(), callbackData.language);
 
-				await fetch(
-					`https://api.telegram.org/bot${env.BOT_TOKEN}/answerCallbackQuery`,
-					{
-						method: "POST",
-						headers: { "Content-Type": "application/json" },
-						body: JSON.stringify({
-							callback_query_id: callbackId,
-							text: `Selected language: ${data}`,
-							show_alert: false,
-						}),
+					await fetch(
+						`https://api.telegram.org/bot${env.BOT_TOKEN}/answerCallbackQuery`,
+						{
+							method: "POST",
+							headers: { "Content-Type": "application/json" },
+							body: JSON.stringify({
+								callback_query_id: callbackId,
+								text: `Selected language: ${callbackData.language}`,
+								show_alert: false,
+							}),
+						}
+					);
+
+					return new Response("OK");
+				}
+
+				if (callbackData.action === "save_translation") {
+					const key = chatId.toString();
+
+					const word = await env.TEMP_STORAGE.get(key);
+					if (!word) {
+						return new Response("No word found", { status: 400 });
 					}
-				);
 
-				return new Response("OK");
+					const stored = await env.DICTIONARY_STORAGE.get(key);
+					const dictionary = stored ? JSON.parse(stored) : [];
+
+					if (!dictionary.includes(word)) {
+						dictionary.push(word);
+						await env.DICTIONARY_STORAGE.put(key, JSON.stringify(dictionary));
+					}
+
+					await fetch(
+						`https://api.telegram.org/bot${env.BOT_TOKEN}/answerCallbackQuery`,
+						{
+							method: "POST",
+							headers: { "Content-Type": "application/json" },
+							body: JSON.stringify({
+								callback_query_id: callbackId,
+								text: `Saved`,
+								show_alert: false,
+							}),
+						}
+					);
+
+					return new Response("OK");
+				}
 			}
-
 
 			if (!message || !chatId) {
 				return new Response("No message or chatId", { status: 400 });
@@ -63,13 +97,16 @@ export default {
 							text: "List of supported languages:",
 							reply_markup: {
 								inline_keyboard: languagelist
-									.filter(item => item.language !== "RU")
+									.filter((item) => item.language !== "RU")
 									.map((item) => [
-									{
-										text: item.name,
-										callback_data: item.language,
-									},
-								]),
+										{
+											text: item.name,
+											callback_data: JSON.stringify({
+												action: "set_language",
+												language: item.language,
+											}),
+										},
+									]),
 							},
 						}),
 					}
@@ -97,12 +134,30 @@ export default {
 				return new Response("Translation failed", { status: 500 });
 			}
 
+
+			await env.TEMP_STORAGE.put(
+				chatId.toString(),
+				message + '-' + translated.translations[0].text
+			);
+
 			await fetch(`https://api.telegram.org/bot${env.BOT_TOKEN}/sendMessage`, {
 				method: "POST",
 				headers: { "Content-Type": "application/json" },
 				body: JSON.stringify({
 					chat_id: chatId,
 					text: translated.translations[0].text,
+					reply_markup: {
+						inline_keyboard: [
+							[
+								{
+									text: "Save",
+									callback_data: JSON.stringify({
+										action: "save_translation",
+									}),
+								},
+							],
+						],
+					},
 				}),
 			});
 
